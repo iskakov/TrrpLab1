abec5484c46b9ed628cb22bc9f312d84959d9c62
Алексей Искаков <ma3ca3@gmail.com>
2019-12-02 12:30:34 +0500

    добавлена аутентификация

---
+++ Trrp(Lab1)/Auth/WebBrooser.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Trrp_Lab1_.MainForm;
+
+namespace Trrp_Lab1_.Auth
+{
+    /// <summary>
+    /// Логика взаимодействия для WebBrooser.xaml
+    /// </summary>
+    public partial class WebBrooser : Window
+    {
+        public WebBrooser(string uri)
+        {
+            InitializeComponent();
+            web.Source = new Uri(uri);
+        }
+
+        private void web_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            
+            var s = web.Source.Fragment.Split('=');
+            if (s.Count() == 2)
+            {
+                WebRequest request = WebRequest.Create("https://oauth.vk.com/access_token?client_id=7230625&client_secret=hxHZWOQDwR3viGHDMq94&code=" + s[1]);
+                request.Method = "GET";
+                WebResponse response = request.GetResponse();
+                string res = "";
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        res = reader.ReadToEnd();
+                    }
+                }
+                response.Close();
+               
+                var obj = JsonConvert.DeserializeObject<JObject>(res);
+                var s23 = obj.Value<string>("access_token");
+                if (s23 != null)
+                {
+                    var form = new MainWin(s23);
+                    form.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("НЕ достаточно прав");
+                    var formAuth = new MainWindow();
+                    formAuth.Show();
+                    this.Close();
+                }
+            }
+           
+        }
+    }
+}
--- Trrp(Lab1)/MainWindow.xaml.cs
+++ Trrp(Lab1)/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trrp_Lab1_.Auth;
 
 namespace Trrp_Lab1_
 {
@@ -23,6 +24,16 @@ namespace Trrp_Lab1_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var s = "https://oauth.vk.com/authorize?client_id=7230625&display=page&scope=friends&response_type=code&v=5.103";
+            var forem = new WebBrooser(s);
+            forem.Show();
+            this.Close();
+                var s1=" https://oauth.vk.com/access_token?client_id=7230625&client_secret=hxHZWOQDwR3viGHDMq94&code=cd4f424793dd601fcb";
         }
     }
 }
